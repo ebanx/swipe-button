13916df010f2528f035f0edd0bffd7f3075a0633
Leandro Ferreira <lehen01@gmail.com>
2017-05-25 16:39:35 -0300

    Configuring Travis

---
+++ gradle/buildWithTravis.sh
@@ -0,0 +1,9 @@
+if [ "$TRAVIS_TAG" == "" ]; then
+  echo -e 'Build Branch'
+  ./gradlew build
+elif [ "$TRAVIS_TAG" != "" ]; then
+  echo -e 'Build Branch for tag: Tag ['$TRAVIS_TAG']'
+  ./gradlew -PbintrayUser="${bintrayUser}" -PbintrayKey="${bintrayKey}" build bintrayUpload --stacktrace --info
+else
+  echo -e 'WARN: Should not be here ./gradlew clean'
+fi
