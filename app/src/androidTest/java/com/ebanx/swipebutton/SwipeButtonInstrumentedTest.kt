c57333a60d258aad3ecb7aab9076cd4edc110a5f
Leandro Ferreira <lehen01@gmail.com>
2017-08-03 12:04:39 -0300

    updating libs

--- app/src/androidTest/java/com/ebanx/swipebutton/SwipeButtonInstrumentedTest.kt
+++ app/src/androidTest/java/com/ebanx/swipebutton/SwipeButtonInstrumentedTest.kt
@@ -1,7 +1,11 @@
 package com.ebanx.swipebutton
 
+import android.app.KeyguardManager
+import android.content.Context
 import android.support.test.rule.ActivityTestRule
+import android.view.WindowManager
 import com.ebanx.swipebtn.SwipeButton
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -10,6 +14,23 @@ class SwipeButtonInstrumentedTest {
     @Rule
     @JvmField
     val activityTest = ActivityTestRule(MainActivity::class.java)
+
+    @Suppress("DEPRECATION")
+    @Before
+    fun setUp() {
+        activityTest.activity.runOnUiThread {
+            val mKG = activityTest.activity.getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+            val mLock = mKG.newKeyguardLock(Context.KEYGUARD_SERVICE)
+            mLock.disableKeyguard()
+
+            //turn the screen on
+            activityTest.activity.window.addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD or
+                    WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
+                    WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON or
+                    WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON or
+                    WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON)
+        }
+    }
 
     @Test
     fun shouldSetEnabledAfterSwipe() {
@@ -34,7 +55,7 @@ class SwipeButtonInstrumentedTest {
     fun shouldCreateButtonCorrectly() {
         test { } result {
             checkButtonIsDisabled(activityTest.activity.findViewById(R.id.swipe_btn_disabled) as SwipeButton)
-            checkButtonIsEnabled(activityTest.activity.findViewById(R.id.swipe_btn_enabled) as SwipeButton)
+            checkButtonIsEnabled(activityTest.activity.findViewById(R.id.swipeBtnEnabled) as SwipeButton)
             shouldBeginWithRightText("SWIPE")
         }
     }
---
+++ app/src/main/java/com/ebanx/swipebutton/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.ebanx.swipebutton
+
+import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        swipeBtnEnabled.background = ContextCompat.getDrawable(this, R.drawable.shape_button2)
+        swipeBtnEnabled.setSlidingButtonBackground(ContextCompat.getDrawable(this, R.drawable.shape_rounded2))
+        swipeBtnEnabled.setInnerTextPadding(10, 10, 10, 10)
+
+        swipeBtnEnabled.setOnStateChangeListener { active ->
+            Toast.makeText(this@MainActivity, "State: " + active, Toast.LENGTH_SHORT).show()
+            if (active) {
+                swipeBtnEnabled.setButtonBackground(ContextCompat.getDrawable(this@MainActivity, R.drawable.shape_button))
+            } else {
+                swipeBtnEnabled.setButtonBackground(ContextCompat.getDrawable(this@MainActivity, R.drawable.shape_button3))
+            }
+        }
+
+        swipeNoState.setOnActiveListener { Toast.makeText(this@MainActivity, "Active!", Toast.LENGTH_SHORT).show() }
+
+        toggleBtn.setOnClickListener {
+            if (!swipeBtnEnabled.isActive) {
+                swipeBtnEnabled.toggleState()
+            }
+        }
+
+    }
+
+}
